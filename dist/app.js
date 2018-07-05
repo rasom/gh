f8b730a2e2651d43cc14dc24a2ca72d22644b613
Ryan Garant <ryantgarant@gmail.com>
2018-07-04 21:49:21 -0700

    Add MVP for listing Issues

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,33 @@
 'use strict'
 Object.defineProperty(exports, '__esModule', { value: true })
 var yargs = require('yargs')
+var graphql_request_1 = require('graphql-request')
+var client = new graphql_request_1.GraphQLClient(
+  'https://api.github.com/graphql',
+  {
+    headers: {
+      Authorization: 'Bearer '
+    }
+  }
+)
 exports.run = function () {
   var parsedArgs = yargs
     .command({
-      command: 'issue [--list|-l]',
+      command: 'issue [--list|-l] [--owner|-o]',
       aliases: ['is'],
       desc: 'List issues from Github repository',
       handler: function (argv) {
-        console.log('argv', argv)
+        if (argv.l || argv.list) {
+          var query =
+            'query($owner: String!) {\n            repository(owner:$owner, name:"gh") {\n              issues(last:100, states:OPEN) {\n                edges {\n                  node {\n                    title\n                    url\n                  }\n                }\n              }\n            }\n          }'
+          var variables = {
+            owner: argv.owner || argv.o
+          }
+          client.request(query, variables).then(function (data) {
+            return console.log('data', data.repository.issues.edges)
+          })
+          console.log('list')
+        }
       }
     })
     .help().argv
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,43 @@
 import * as yargs from 'yargs'
+import { GraphQLClient } from 'graphql-request'
+
+const client = new GraphQLClient('https://api.github.com/graphql', {
+  headers: {
+    Authorization: 'Bearer ',
+  },
+})
 
 export const run = (): void => {
   const parsedArgs = yargs
     .command({
-      command: 'issue [--list|-l]',
+      command: 'issue [--list|-l] [--owner|-o]',
       aliases: ['is'],
       desc: 'List issues from Github repository',
       handler: argv => {
-        console.log('argv', argv)
+        if (argv.l || argv.list) {
+          const query = `query($owner: String!) {
+            repository(owner:$owner, name:"gh") {
+              issues(last:100, states:OPEN) {
+                edges {
+                  node {
+                    title
+                    url
+                  }
+                }
+              }
+            }
+          }`
+
+          const variables = {
+            owner: argv.owner || argv.o,
+          }
+
+          client
+            .request(query, variables)
+            .then(data => console.log('data', data.repository.issues.edges))
+
+          console.log('list')
+        }
       },
     })
     .help().argv
