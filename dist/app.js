79e34a74092abfb2a4e210bd3eb5daebd43111a8
Ryan Garant <ryantgarant@gmail.com>
2018-07-04 21:15:03 -0700

    Setup yargs option parser

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,16 @@
-import * as yargs from 'yargs'
-
-export const run = (): void => {
-  const parsedArgs = yargs
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+var yargs = require('yargs')
+exports.run = function () {
+  var parsedArgs = yargs
     .command({
       command: 'issue [--list|-l]',
       aliases: ['is'],
       desc: 'List issues from Github repository',
-      handler: argv => {
+      handler: function (argv) {
         console.log('argv', argv)
-      },
+      }
     })
     .help().argv
-
   console.log('parsedArgs', parsedArgs)
 }
--- index.js
+++ index.js
@@ -3,8 +3,5 @@
 const app = require('./dist/app')
 
 if (process.stdin.isTTY) {
-  console.log('process.argv.slice(2)', process.argv.slice(2))
-  app.run(process.argv.slice(2))
-} else {
-  app.runFromPipedData()
+  app.run()
 }
